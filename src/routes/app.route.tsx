705d5f3e63ccaabbd5e49f33f5e74be6dd39dc2d
mpgxc <mpgx5.c@gmail.com>
2020-10-04 20:59:43 -0300

    feat: add style for novoButton

--- src/routes/app.route.tsx
+++ src/routes/app.route.tsx
@@ -1,26 +1,65 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { AntDesign } from "@expo/vector-icons";
+import { AntDesign, Ionicons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+import NovoButton from "../components/NovoButton";
 
 import Feed from "../screens/Feed";
-import Profile from "../screens/Feed";
+import Favorites from "../screens/Favorites";
 import Colors from "../constants/colors";
+
+const Hello = () => (
+  <View
+    style={{
+      flex: 1,
+      backgroundColor: "#F5F5F5",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Text style={{ color: "#333", fontSize: 150 }}>Hello</Text>
+  </View>
+);
 
 const App: React.FC = () => {
   const { Navigator, Screen } = createBottomTabNavigator();
+
+  const zoomAnimate = (focused: boolean, size: number) =>
+    focused ? size + 3 : size;
+
   return (
     <Navigator
+      screenOptions={({ route, navigation }) => ({
+        tabBarIcon: ({ color, size, focused }) => {
+          if (route.name === "Novo") {
+            return (
+              <NovoButton
+                size={size}
+                primaryColor={color}
+                secondColor="#c1bccc"
+                focused={focused}
+                onPress={() => {
+                  navigation.navigate("Novo");
+                }}
+              />
+            );
+          }
+        },
+      })}
       tabBarOptions={{
         showLabel: false,
 
         style: {
           elevation: 0,
-          shadowOpacity: 1,
+          shadowOpacity: 0,
           height: 50,
         },
 
-        inactiveBackgroundColor: "#FAFAFC",
-        activeBackgroundColor: "#EBEBF5",
+        tabStyle: {
+          justifyContent: "center",
+        },
+
+        keyboardHidesTabBar: true,
         inactiveTintColor: "#c1bccc",
         activeTintColor: Colors.blackTheme.primary,
       }}
@@ -28,8 +67,12 @@ const App: React.FC = () => {
       <Screen
         options={{
           tabBarLabel: "Feed",
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="home" size={size} color={color} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <AntDesign
+              name="home"
+              size={zoomAnimate(focused, size)}
+              color={color}
+            />
           ),
         }}
         name="Feed"
@@ -39,23 +82,48 @@ const App: React.FC = () => {
       <Screen
         options={{
           tabBarLabel: "Favoritos",
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="hearto" size={size} color={color} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <AntDesign
+              name="hearto"
+              size={zoomAnimate(focused, size)}
+              color={color}
+            />
           ),
         }}
         name="Favoritos"
-        component={Feed}
+        component={Favorites}
+      />
+
+      <Screen name="Novo" component={Hello} />
+
+      <Screen
+        options={{
+          tabBarLabel: "Notifications",
+          tabBarIcon: ({ color, size, focused }) => (
+            <Ionicons
+              name="ios-notifications-outline"
+              size={zoomAnimate(focused, size + 5)}
+              color={color}
+            />
+          ),
+        }}
+        name="Notifications"
+        component={Hello}
       />
 
       <Screen
         options={{
           tabBarLabel: "Profile",
-          tabBarIcon: ({ color, size }) => (
-            <AntDesign name="setting" size={size} color={color} />
+          tabBarIcon: ({ color, size, focused }) => (
+            <AntDesign
+              name="setting"
+              size={zoomAnimate(focused, size)}
+              color={color}
+            />
           ),
         }}
         name="Profile"
-        component={Profile}
+        component={Hello}
       />
     </Navigator>
   );
